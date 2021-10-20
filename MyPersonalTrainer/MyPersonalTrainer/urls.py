8213c8a8640027c9de5dcf9b3caa4769a811ba3a
Your Name <you@example.com>
2021-10-20 22:24:46 +0200

    add signUp app

--- MyPersonalTrainer/MyPersonalTrainer/urls.py
+++ MyPersonalTrainer/MyPersonalTrainer/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('signIn/',include('signIn.urls'))
+    path('signIn/',include('signIn.urls')),
+    path('signUp/',include('signUp.urls'))
+
 ]
