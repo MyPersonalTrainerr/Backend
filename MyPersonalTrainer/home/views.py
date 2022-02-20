e1adc88958ad422d350071f03bb290c2e27ffe0d
Ahmed Abdel Aal <eng.ahmedhussein89@hotmail.com>
2022-02-20 19:08:29 +0200

    Format home directory

--- MyPersonalTrainer/home/views.py
+++ MyPersonalTrainer/home/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth import login as auth_login
+
 from .forms import SignUpForm
-# Create your views here.
+
+
 def home(request):
     return render (request,'home/index.html')
 
-
-
-# Create your views here.
 def signUp(request):
     form=SignUpForm()
     if request.method=='POST':
@@ -16,4 +15,3 @@ def signUp(request):
             user=form.save()
             auth_login(request,user)
             return redirect('home')
-    # return render(request,'home/index.html',{'form':form})
