4e11b5daa98a8aae5ea941b3c9b552bef42d2fa4
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2022-04-17 05:58:59 +0200

    Format settings file

--- MyPersonalTrainer/MyPersonalTrainer/settings.py
+++ MyPersonalTrainer/MyPersonalTrainer/settings.py
@@ -151,11 +151,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-MEDIA_ROOT = os.path.join(BASE_DIR,'uploadedFiles')
-MEDIA_URL= '/uploadedFiles/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploadedFiles')
+MEDIA_URL = '/uploadedFiles/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
