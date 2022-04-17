59f54e2806bcd8762db6e59124cd02068174fbe5
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2022-04-17 05:19:08 +0200

    Create post models

--- MyPersonalTrainer/blogs/admin.py
+++ MyPersonalTrainer/blogs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from blogs.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_at')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
--- MyPersonalTrainer/blogs/models.py
+++ MyPersonalTrainer/blogs/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+User = get_user_model()
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    slug       = models.SlugField(max_length=128, unique=True)
+    title      = models.CharField(max_length=128, unique=True)
+    content    = models.TextField()
+    author     = models.ForeignKey(User, on_delete= models.CASCADE, related_name='blog_posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status     = models.IntegerField(choices=STATUS, default=0)
+    # tags
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
