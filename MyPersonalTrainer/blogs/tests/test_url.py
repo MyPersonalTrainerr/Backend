194f079d859c811f66cb947f754d1a6eacb6a8bc
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2022-04-17 05:24:11 +0200

    Add test for blog urls

--- MyPersonalTrainer/blogs/tests/test_url.py
+++ MyPersonalTrainer/blogs/tests/test_url.py
@@ -1,9 +1,20 @@
+from unittest.mock import patch
+
 from django.test import TestCase
-from django.urls import resolve
-from blogs.views import blogs_view
+from django.urls import resolve, reverse
+
+from blogs.views import (
+    blogs_view,
+    blog_detail_view
+)
 
 
 class TestUrl(TestCase):
-    def test_blog_view_function(self):
-        resolver = resolve('/blogs')
+    
+    def test_blogs_view_function(self):
+        resolver = resolve(reverse('blogs_view'))
         self.assertEqual(resolver.func, blogs_view)
+
+    def test_blog_detail_view_function(self):
+        resolver = resolve(reverse('blog_detail', args={"slug": "a-slug"}))
+        self.assertEqual(resolver.func, blog_detail_view)
